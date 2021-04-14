887517b9cf878f114230c6aad70fcd440bbc1d46
ginareeena <ginamoffit@gmail.com>
2021-04-14 17:10:16 -0400

    adding sumZero solution

--- udemyAlgos/pointers/sumZero.js
+++ udemyAlgos/pointers/sumZero.js
@@ -5,14 +5,23 @@
 // if there are none it should return undefined
 
 function sumZero(numArr) {
-  let leftIdx = 0;
-  let rightIdx = numArr.length - 1;
-  while (leftIdx < rightIdx) {
-    if (numArr[leftIdx] + numArr[rightIdx] === 0) {
-      return [numArr[leftIdx], numArr[rightIdx]];
+  let left = 0;
+  let right = numArr.length - 1;
+  while (left < right) {
+    let sum = numArr[left] + numArr[right];
+    if (sum === 0) {
+      return [numArr[left], numArr[right]];
+    } else if (sum > 0) {
+      // if the number is too large make the biggest number smaller
+      right--;
+    } else {
+      left++;
     }
   }
   return undefined;
 }
 
-console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]));
+//Time Complexity= O(n)
+//Space Complexity= O(1)
+
+console.log(sumZero([-4, -2, -1, 0, 1, 2, 3]));
