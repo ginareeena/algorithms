05c61bc50f3edbfc13f4db6737efe1536f54be19
ginareeena <ginamoffit@gmail.com>
2021-04-14 18:01:24 -0400

    adding solution to count unique values

--- udemyAlgos/pointers/countUniqueValues.js
+++ udemyAlgos/pointers/countUniqueValues.js
@@ -2,7 +2,7 @@
 //unique values in an array
 //returns 0 if array is empty
 
-// My Solution with a for loop:
+// My Solution with a for loop and freq counter:
 // Big O= O(n) linear
 
 function countUniqueValuesLoop(arr) {
@@ -55,6 +55,23 @@ function countUniqueValues(arr) {
   }
 }
 
+//Udemy's solution
+
+// don't need to increment j in a for loop with j
+function countUniqueValuesAlt(arr) {
+  let i = 0;
+  if (arr.length === 1) {
+    return 0;
+  }
+  for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+      i++;
+      arr[i] == arr[j];
+    }
+  }
+  return i + 1;
+}
+
 // console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 5, 6, 7])); // 7 <- counts 4 once
 // console.log(countUniqueValues([1, 2, 3, 5, 6, 7])); // 6 <- counts 4 once
 // console.log(countUniqueValues([1, 2, 3, 4, 5, 6, 6, 7])); // 7 <- counts 4 once
@@ -62,3 +79,7 @@ function countUniqueValues(arr) {
 // console.log(countUniqueValuesLoop([1, 2, 3, 4, 4, 4, 5, 6, 7])); // 7
 // console.log(countUniqueValuesLoop([1, 2, 3, 5, 6, 7])); // 6
 // console.log(countUniqueValuesLoop([1, 2, 3, 4, 5, 6, 6, 7])); //
+
+// console.log(countUniqueValuesAlt([1, 2, 3, 4, 4, 4, 5, 6, 7])); // 7
+// console.log(countUniqueValuesAlt([1, 2, 3, 5, 6, 7])); // 6
+// console.log(countUniqueValuesAlt([1, 2, 3, 4, 5, 6, 6, 7])); //
