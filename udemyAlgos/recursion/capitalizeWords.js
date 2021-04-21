eff133d1b592af888b7c91bca67bfc72a6a72fc3
ginareeena <ginamoffit@gmail.com>
2021-04-21 19:01:21 -0400

    adds capitalizeWords solution

--- udemyAlgos/recursion/capitalizeWords.js
+++ udemyAlgos/recursion/capitalizeWords.js
@@ -1,15 +1,23 @@
 // write a recursive function that given an array of words
 // returns a new array with each word capitalized
 
-function capitalizeWordsMin(arr) {
+function capitalizeWordsMine(arr) {
   let newArr = [];
-  if (arr.length === 1) {
-    return [arr[0].toUpperCase()];
-  }
   let newWord = arr[0].toUpperCase();
+
+  if (arr.length === 1) {
+    return [newWord];
+  }
   newArr.push(newWord);
-  return newArr.concat(capitalizeWordsMin(arr.slice(1)));
+  return newArr.concat(capitalizeWordsMine(arr.slice(1)));
 }
+
+//your solution slices off the first word and pushes it to newArr..
+//and concats result of the recursive call on the old arr minus that first element
+
+//udemy's makes recursive call on the old arr minus the last element
+//then pushes the last word uppercased to the result
+// and returns it
 
 //udemy's solution
 
@@ -17,7 +25,13 @@ function capitalizeWords(array) {
   if (array.length === 1) {
     return [array[0].toUpperCase()];
   }
+  //recursive call on the array minus the last element
+  // array: ["i", "like", "cats"]
+  // res equals recursive call on: ["i", "like"]
   let res = capitalizeWords(array.slice(0, -1));
+
+  // slices last element in array, makes upper case- pushes
+  //
   res.push(array.slice(array.length - 1)[0].toUpperCase());
   return res;
 }
@@ -26,6 +40,16 @@ let words = ["i", "am", "a", "cat"];
 // console.log(capitalizeWords(words));
 
 console.log("my solution:");
-console.log(capitalizeWordsMin(words));
+console.log(capitalizeWordsMine(words));
 console.log("udemys solution:");
 console.log(capitalizeWords(words));
+
+// let cat = "cat";
+//cat.slice(1) // "at"
+//cat.slice(0,-1) // "ca"
+
+//These two are the same:
+//cat.slice(-1) // "t"
+//cat.slice(cat.length-1) // "t"
+
+//cat.slice(2) // "t"
