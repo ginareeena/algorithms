1b0bccb32bf35cb103f2e8db994980cfdc4471d0
ginareeena <ginamoffit@gmail.com>
2021-04-22 22:06:33 -0400

    adds solution to naive string search

---
+++ udemyAlgos/09-naive string search/naiveStringSearch.js
@@ -0,0 +1,30 @@
+//write a function that takes a string and a search
+
+function naiveStringSearch(haystack, needle) {
+  let count = 0;
+  let hIdx = 0;
+  let nIdx = 0;
+  if (needle.length > haystack.length) {
+    return 0;
+  }
+  while (hIdx < haystack.length) {
+    if (haystack[hIdx] === needle[nIdx]) {
+      if (nIdx === needle.length - 1) {
+        count++;
+        hIdx++;
+        nIdx = 0;
+      } else {
+        hIdx++;
+        nIdx++;
+      }
+    } else {
+      hIdx++;
+      nIdx = 0;
+    }
+  }
+  return count;
+}
+
+let word = "wowomgzomg";
+console.log(naiveStringSearch(word, "omg"));
+console.log(naiveStringSearch(word, "omgggggggggggggggg"));
