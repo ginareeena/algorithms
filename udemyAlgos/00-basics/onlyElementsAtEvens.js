91c60b24165e29fc84f987e3a2c0d1f072e5589e
ginareeena <ginamoffit@gmail.com>
2021-05-03 21:49:57 -0400

    adds optimization for onlyElementsAtEvens.js

--- udemyAlgos/00-basics/onlyElementsAtEvens.js
+++ udemyAlgos/00-basics/onlyElementsAtEvens.js
@@ -1,6 +1,16 @@
-//function that returns on the elements from the evens index of original array
+//function that returns only the elements from the evens index of original array
 
+//most optimized don't need to loop through every element can just increment i += 2
 function onlyElementsAtEvenIndex(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i += 2) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+
+// my old solution
+function onlyElementsAtEvenIndexAlt2(array) {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
     if (i % 2 === 0) {
@@ -10,6 +20,7 @@ function onlyElementsAtEvenIndex(array) {
   return newArray;
 }
 
+//udemy's solution
 function onlyElementsAtEvenIndexAlt(array) {
   //initializes an array half the length
   var newArray = Array(Math.ceil(array.length / 2));
@@ -32,3 +43,16 @@ console.log("catsArr", catsArr); //[<2 empty items>]
 console.log(onlyElementsAtEvenIndex(cats)); // ["kiki", "salem"]
 // their solution
 console.log(onlyElementsAtEvenIndexAlt(cats)); //["kiki", "salem"]
+
+// function onlyElementsAtEvenIndexAlt(array) {
+//   //initializes an array half the length
+//   var newArray = Array(Math.ceil(array.length / 2));
+//   for (var i = 0; i < array.length; i++) {
+//   if (i % 2 === 0) {
+//   newArray[i / 2] = array[i];
+//   }
+//   }
+//   return newArray;
+//   }
+//   Instead of looping one at a time and then doing mod on i, you can just do this
+//   for (let i = 0; i < array.length; i += 2)
