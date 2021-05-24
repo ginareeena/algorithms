d4b78eb9ac31d95b1f59630450fa1ec133b39f03
ginareeena <ginamoffit@gmail.com>
2021-05-24 14:49:40 -0400

    cleans up merge sort

--- udemyAlgos/08- intermediate sorting algos/mergesort.js
+++ udemyAlgos/08- intermediate sorting algos/mergesort.js
@@ -1,48 +1,75 @@
 //helper function to merge arrays
 
-function mergeArr(arrA, arrB) {
-  let newArr = [];
+function merge(arrA, arrB) {
+  let results = [];
   let i = 0;
   let j = 0;
   while (i < arrA.length && j < arrB.length) {
-    let currA = arrA[i];
-    let currB = arrB[j];
-    console.log("A, B, newArr", currA, currB, newArr);
-
-    currA < currB ? newArr.push(currA, currB) : newArr.push(currB, currA);
+    if (arrB[j] > arrA[i]) {
+      results.push(arrA[i]);
+      i++;
+    } else {
+      results.push(arrB[j]);
+      j++;
+    }
+  }
+  while (i < arrA.length) {
+    results.push(arrA[i]);
     i++;
+  }
+  while (j < arrB.length) {
+    results.push(arrB[j]);
     j++;
   }
-  if (j < arrB.length) {
-    let lastElem = newArr[newArr.length - 1];
-    while (j < arrB.length) {
-      if (lastElem < arrB[j]) {
-        newArr.push(arrB[j]);
-        j++;
-      } else {
-        let temp = lastElem;
-        newArr[newArr.length - 1] = arrB[j];
-        newArr.push(temp);
-        j++;
-      }
-    }
-  } else if (i < arrA.length) {
-    let lastElem = newArr[newArr.length - 1];
-    while (i < arrA.length) {
-      if (lastElem < arrA[i]) {
-        newArr.push(arrA[i]);
-        i++;
-      } else {
-        let temp = lastElem;
-        newArr[newArr.length - 1] = arrA[i];
-        newArr.push(temp);
-        i++;
-      }
-    }
-  }
-  return newArr;
+  return results;
 }
 
-console.log(mergeArr([1, 2, 4], [2, 5]));
-console.log(mergeArr([1, 2, 4, 6], [2, 5]));
-console.log(mergeArr([2, 2, 4, 6], [1, 5]));
+console.log(merge([1, 2, 4], [2, 5]));
+console.log(merge([1, 2, 4, 6], [2, 5]));
+console.log(merge([2, 2, 4, 6], [1, 5]));
+console.log(merge([], [8, 10]));
+
+// initial solution:
+
+// function mergeArrFirst(arrA, arrB) {
+//   let newArr = [];
+//   let i = 0;
+//   let j = 0;
+//   while (i < arrA.length && j < arrB.length) {
+//     let currA = arrA[i];
+//     let currB = arrB[j];
+//     console.log("A, B, newArr", currA, currB, newArr);
+
+//     currA < currB ? newArr.push(currA, currB) : newArr.push(currB, currA);
+//     i++;
+//     j++;
+//   }
+//   if (j < arrB.length) {
+//     let lastElem = newArr[newArr.length - 1];
+//     while (j < arrB.length) {
+//       if (lastElem < arrB[j]) {
+//         newArr.push(arrB[j]);
+//         j++;
+//       } else {
+//         let temp = lastElem;
+//         newArr[newArr.length - 1] = arrB[j];
+//         newArr.push(temp);
+//         j++;
+//       }
+//     }
+//   } else if (i < arrA.length) {
+//     let lastElem = newArr[newArr.length - 1];
+//     while (i < arrA.length) {
+//       if (lastElem < arrA[i]) {
+//         newArr.push(arrA[i]);
+//         i++;
+//       } else {
+//         let temp = lastElem;
+//         newArr[newArr.length - 1] = arrA[i];
+//         newArr.push(temp);
+//         i++;
+//       }
+//     }
+//   }
+//   return newArr;
+// }
