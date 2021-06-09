180d791b7d209247577986ae9b5134878831a3b1
ginareeena <ginamoffit@gmail.com>
2021-06-09 18:19:10 -0400

    cleans up validateSequence solution

--- algoExpert/validateSequence.js
+++ algoExpert/validateSequence.js
@@ -7,6 +7,31 @@
 //Ex: [1,3,4] and [2,4] are subsequences of [1,2,3,4]
 
 function isValidSubsequence(array, sequence) {
+  console.log("array", array);
+  console.log("sequence", sequence);
+
+  if (sequence.length > array.length) {
+    return false;
+  }
+
+  let aIdx = 0;
+  let sIdx = 0;
+
+  while (aIdx < array.length && sIdx < sequence.length) {
+    let arrVal = array[aIdx];
+    let seqVal = sequence[sIdx];
+    if (arrVal === seqVal) sIdx++;
+    aIdx++;
+  }
+  if (sIdx === sequence.length) {
+    return true;
+  }
+  return false;
+}
+
+//cleaner solution:
+
+function isValidSubsequence(array, sequence) {
   if (sequence.length > array.length) {
     return false;
   }
@@ -18,8 +43,5 @@ function isValidSubsequence(array, sequence) {
     if (array[aIdx] === sequence[sIdx]) sIdx++;
     aIdx++;
   }
-  if (sIdx === sequence.length) {
-    return true;
-  }
-  return false;
+  return sIdx === sequence.length;
 }
