8fbab2d45964f451c56ac3342faa480aede2b61f
ginareeena <ginamoffit@gmail.com>
2021-06-16 01:12:27 -0400

    cleans up code

--- pramp/frontEnd/dropDownMenuII/dropDown.js
+++ pramp/frontEnd/dropDownMenuII/dropDown.js
@@ -2,11 +2,11 @@ let dropDownContent = document.getElementsByClassName("dropdown-content")[0];
 
 function dropdownClick() {
   dropDownContent.style.display = "block";
-  console.log("you clicked me!");
+  // console.log("you clicked me!");
 }
 
 window.onclick = function (e) {
-  if (!e.target.matches(".dropBtn")) {
+  if (!e.target.matches(".dropbtn")) {
     dropDownContent.style.display = "none";
   }
 };
