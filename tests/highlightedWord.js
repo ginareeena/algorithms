1dd7b38ba61e59118a488b54242b42c0cb656abc
Gina Moffit <gina.moffit@teladoc.com>
2021-10-13 09:53:10 -0400

    adds solution to highlightedWord

---
+++ tests/highlightedWord.js
@@ -0,0 +1,43 @@
+// write a function that takes a query and a result
+// and returns the query highlighted if in the result
+
+// highlightedResult("read", "reading is fun")
+// should return "<b>read</b>ing is fun"
+
+function getResult(query, result){
+    if(!result.includes(query)){
+      return result
+    }  
+
+    let highlightedResult= ""
+    let idxs= []
+    let endIdx= query.length
+  
+    for(let i=0;i<result.length;i++){
+      let currSlice= result.slice(i)
+      console.log('currSlice', currSlice)
+      if(!currSlice.includes(query)){
+        break
+      }
+      let currWord= result.slice(i, i+endIdx)
+   
+      if(currWord === query){
+      idxs.push(i)
+      }
+    }
+  
+     for(let i=0;i<result.length;i++){
+      if(idxs.includes(i)){
+        highlightedResult += `<b>${result.slice(i, i+endIdx)}</b>`
+      i+= endIdx - 1
+      }
+      else {
+        highlightedResult += result[i]
+      }
+     }
+  
+    return highlightedResult
+  }
+  
+  
+  console.log(getResult("read", "reading is fun reading"))
