ab21d660ece141abb2dde583c4e4ef439e40996b
Gina Moffit <gina.moffit@teladoc.com>
2021-10-19 16:51:42 -0400

    adds solution to meetingsPagination.js

--- tests/Async/Await/getFishChips.js
+++ tests/Async/Await/getFishChips.js
@@ -25,25 +25,35 @@
 //The first way to remove the nesting of these fetch calls is to use what is known as promise chaining. Promise chaining can be achieved by simply returning another promise from within a call to Promise.then. The following example shows how to do this:
 
 // We have to get chips after we get fish...
-getFishAndChips = () => {
-  fetch(this.fishApiUrl) // Request fish
-    .then((response) => response.json())
-    .then((fish) => {
-      this.fish = fish;
+// getFishAndChips = () => {
+//   fetch(this.fishApiUrl) // Request fish
+//     .then((response) => response.json())
+//     .then((fish) => {
+//       this.fish = fish;
 
-      const fishIds = fish.map((fish) => fish.id);
+//       const fishIds = fish.map((fish) => fish.id);
 
-      return fetch(
-        // Request chips using fish ids
-        this.chipsApiUrl,
-        {
-          method: "POST",
-          body: JSON.stringify({fishIds}),
-        }
-      );
-    })
-    .then((response) => response.json())
-    .then((chips) => {
-      this.chips = chips;
-    });
-};
+//       return fetch(
+//         // Request chips using fish ids
+//         this.chipsApiUrl,
+//         {
+//           method: "POST",
+//           body: JSON.stringify({fishIds}),
+//         }
+//       );
+//     })
+//     .then((response) => response.json())
+//     .then((chips) => {
+//       this.chips = chips;
+//     });
+// };
+
+
+const getFishAndChips = async (fishApiUrl) => {
+  const fish = await fetch(fishApiUrl).then(response => response.json());
+
+  const fishIds = fish.map(fish => fish.id),
+  chipReqOpts = { method: 'POST', body: JSON.stringify({ fishIds }) };
+
+  const chips = await fetch(chipsApiUrl, chipReqOpts).then(response => response.json());
+}
--- tests/csty/5-form.js
+++ tests/csty/5-form.js
@@ -6,3 +6,156 @@
 // Phone = 8885559999
 
 // You are free to add classes and styles, but make sure you leave the element ID's as they are. Submit your code once it is complete and our system will validate your output.
+
+import React, {useState} from "react";
+import ReactDom from "react-dom";
+
+const style = {
+  table: {
+    borderCollapse: "collapse",
+  },
+  tableCell: {
+    border: "1px solid gray",
+    margin: 0,
+    padding: "5px 10px",
+    width: "max-content",
+    minWidth: "150px",
+  },
+  form: {
+    container: {
+      padding: "20px",
+      border: "1px solid #F0F8FF",
+      borderRadius: "15px",
+      width: "max-content",
+      marginBottom: "40px",
+    },
+    inputs: {
+      marginBottom: "5px",
+    },
+    submitBtn: {
+      marginTop: "10px",
+      padding: "10px 15px",
+      border: "none",
+      backgroundColor: "lightseagreen",
+      fontSize: "14px",
+      borderRadius: "5px",
+    },
+  },
+};
+
+//PhoneBookForm ({addEntryToPhoneBook})
+
+
+function PhoneBookForm({addEntryToPhoneBook}) {
+  const [userFirstName, setUserFirstName] = useState("");
+  const [userLastname, setUserLastName] = useState("");
+  const [userPhone, setUserPhone] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    addEntryToPhoneBook([userFirstName, userLastname, userPhone]);
+
+    setUserFirstName("");
+    setUserLastName("");
+    setUserPhone("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit} style={style.form.container}>
+      <label>First name:</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        className="userFirstname"
+        name="userFirstname"
+        type="text"
+        value={userFirstName}
+        onChange={(e) => {
+          setUserFirstName(e.target.value);
+        }}
+      />
+      <br />
+      <label>Last name:</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        className="userLastname"
+        name="userLastname"
+        type="text"
+        value={userLastname}
+        onChange={(e) => {
+          setUserLastName(e.target.value);
+        }}
+      />
+      <br />
+      <label>Phone:</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        className="userPhone"
+        name="userPhone"
+        type="text"
+        value={userPhone}
+        onChange={(e) => {
+          setUserPhone(e.target.value);
+        }}
+      />
+      <br />
+      <input
+        style={style.form.submitBtn}
+        className="submitButton"
+        type="submit"
+        value="Add User"
+      />
+    </form>
+  );
+}
+
+function InformationTable(props) {
+  return (
+    <table style={style.table} className="informationTable">
+      <thead>
+        <tr>
+          <th style={style.tableCell}>First name</th>
+          <th style={style.tableCell}>Last name</th>
+          <th style={style.tableCell}>Phone</th>
+        </tr>
+      </thead>
+    </table>
+  );
+}
+
+function Entries({phoneBook = []}) {
+  return (
+    <thead>
+      {phoneBook.map((entry, index) => (
+        <tr key={index}>
+          <th style={style.tableCell}>{entry[0]}</th>
+          <th style={style.tableCell}>{entry[1]}</th>
+          <th style={style.tableCell}>{entry[2]}</th>
+        </tr>
+      ))}
+    </thead>
+  );
+}
+
+function Application(props) {
+  const [phoneBook, updatePhoneBook] = useState([
+    ["Coder", "Byte", "8885559999"],
+  ]);
+
+  function addEntryToPhoneBook(newEntry) {
+    updatePhoneBook([...phoneBook, newEntry]);
+  }
+
+  return (
+    <section>
+      <PhoneBookForm addEntryToPhoneBook={addEntryToPhoneBook} />
+      <InformationTable />
+      <Entries phoneBook={phoneBook} />
+    </section>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("root"));
