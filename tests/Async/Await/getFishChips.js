4cb2e01b5f660a9cc43ef522d397e15a2c0b37b9
ginareeena <ginamoffit@gmail.com>
2021-10-19 23:50:20 -0400

    adds solution to getFishandChips.js

--- tests/Async/Await/getFishChips.js
+++ tests/Async/Await/getFishChips.js
@@ -25,25 +25,39 @@
 //The first way to remove the nesting of these fetch calls is to use what is known as promise chaining. Promise chaining can be achieved by simply returning another promise from within a call to Promise.then. The following example shows how to do this:
 
 // We have to get chips after we get fish...
-getFishAndChips = () => {
-  fetch(this.fishApiUrl) // Request fish
-    .then((response) => response.json())
-    .then((fish) => {
-      this.fish = fish;
+// getFishAndChips = () => {
+//   fetch(this.fishApiUrl) // Request fish
+//     .then((response) => response.json())
+//     .then((fish) => {
+//       this.fish = fish;
 
-      const fishIds = fish.map((fish) => fish.id);
+//       const fishIds = fish.map((fish) => fish.id);
 
-      return fetch(
-        // Request chips using fish ids
-        this.chipsApiUrl,
-        {
-          method: "POST",
-          body: JSON.stringify({fishIds}),
-        }
-      );
-    })
-    .then((response) => response.json())
-    .then((chips) => {
-      this.chips = chips;
-    });
+//       return fetch(
+//         // Request chips using fish ids
+//         this.chipsApiUrl,
+//         {
+//           method: "POST",
+//           body: JSON.stringify({fishIds}),
+//         }
+//       );
+//     })
+//     .then((response) => response.json())
+//     .then((chips) => {
+//       this.chips = chips;
+//     });
+// };
+
+//This is great! The code is much more maintainable, succinct, and readable. But we can do even better! With the new async/await syntax that has been brought into JavaScript, we can remove the use of multiple calls to Promise.then entirely! You will see how this can be achieved in the next section.
+
+// Optimal solution:
+getFishAndChips = async () => {
+  const fish = await fetch(this.fishApiUrl).then((response) => response.json());
+
+  const fishIds = fish.map((fish) => fish.id),
+    chipReqOpts = {method: "POST", body: JSON.stringify({fishIds})};
+
+  const chips = await fetch(this.chipsApiUrl, chipReqOpts).then((response) =>
+    response.json()
+  );
 };
